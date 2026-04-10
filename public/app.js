8d59d11325a773514a925bfd09ef9c2b807add23
cosole44 <134920864+cosole44@users.noreply.github.com>
2026-04-10 22:52:53 +0500

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -23,9 +23,8 @@ const statusTemplate    = document.getElementById("status-item-template");
 const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('click', () => {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  
   document.documentElement.setAttribute('data-theme', newTheme);
   localStorage.setItem('theme', newTheme);
 });
